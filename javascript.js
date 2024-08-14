42799c5e5b140afde0ce0a4f49705be0b353bf9d
Carolyn Borcherding <166224066+cborch31@users.noreply.github.com>
2024-08-14 17:18:19 -0400

    try to target individual divs

--- javascript.js
+++ javascript.js
@@ -1,5 +1,8 @@
 console.log('hello world!')
 
+////////////////////////////////////////////////////////
+//operations
+
 let firstNumber = 3;
 let operator = '-';
 let secondNumber = 2;
@@ -9,7 +12,6 @@ const addition = function(a,b){
 };
 
 const subtraction = function(a,b){
-    //console.log(a - b);
     return subtract = a - b;
 };
 
@@ -33,18 +35,28 @@ const operate = function(operator, firstNumber, secondNumber){
     }
 };
 
+///////////////////////////////////////////////////////////
+//making DIVs and buttons
+
 const calcContainer = document.querySelector('#calcContainer');
 const mainPage = document.querySelector('#mainPage');
 let calcArray = [];
 
 document.getElementById('calcContainer');
 
+displayBox = document.createElement('div');
+displayBox.classList.add('displayBox');
+calcContainer.appendChild(displayBox);
+displayBox.textContent = '4 x 5'; //erase later
+
 for (let i = 0; i < 16; i++){
     numbers = document.createElement('div');
     numbers.classList.add('numbers'); //to refer to the group
-    numbers.classList.add('numbers' + i); //to stylistically refer to each individually. I feel really smart ;)
+    numbers.classList.add('numbers' + i); //to stylistically refer to each individually.
     calcContainer.appendChild(numbers);
 }
+
+numbers1.textContent = '1'; //but this doesn't work
 
 cellList = document.querySelectorAll('.numbers'); //cellList is an object + not an array
 
