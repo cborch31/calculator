6e6c13ae78f53826253699958de3885f28c62b18
Carolyn Borcherding <166224066+cborch31@users.noreply.github.com>
2024-08-16 11:53:12 -0400

    make buttons. create display func

--- javascript.js
+++ javascript.js
@@ -37,9 +37,20 @@ const operate = function(operator, firstNumber, secondNumber){
 
 let val = "10";
 
+//get a value
+//check if it is an operand
+//check if displayBox has already received an operand
+
 const displayFunction = function(val){
-    displayBox.textContent = val;
-    return val;
+    if( val === '+' ||
+        val === '-' ||
+        val === '/' ||
+        val === '*'
+    ) { }
+    
+    console.log(displayBox.textContent);
+    // displayBox.textContent += val;
+    // return val;
 };
 
 //updating the displayBox
@@ -48,11 +59,15 @@ const displayFunction = function(val){
 
 ///////////////////////////////////////////////////////////
 //making DIVs and buttons
-// let array = ['4', '4', '*', '5'];
-// let item = array.join(' ');
+let array = ['4', '4', '*', '5'];
+let item = array.join('');
+
+console.log(item);
+
 const calcContainer = document.querySelector('#calcContainer');
 const mainPage = document.querySelector('#mainPage');
 const display = document.querySelector('#display');
+const clear = document.querySelector('#clear');
 let calcArray = [];
 
 document.getElementById('calcContainer');
@@ -62,59 +77,18 @@ displayBox.classList.add('displayBox');
 display.appendChild(displayBox);
 //displayBox.textContent = ''; //erase later
 
-for (let i = 0; i < 15; i++){
-    numbers = document.createElement('button');
-    numbers.classList.add('numbers'); //to refer to the group
-    numbers.classList.add('numbers' + i); //to stylistically refer to each individually.
-    numbers.setAttribute('id', 'btn'+i);
-    calcContainer.appendChild(numbers);
-}
+const btn7 = document.querySelector('#btn7'); //each event listener should do two things: update the display box, send their value to an array
+btn7.addEventListener('click', () => {displayFunction(7)});
 
-clearBtn = document.createElement('button');
-clearBtn.classList.add('clearBtn');
-calcContainer.appendChild(clearBtn);
-clearBtn.textContent = 'Clear';
-
-let box7 = document.getElementsByClassName('numbers')[0];
-box7.textContent = '7';
-// const btn0 = document.getElementById('#btn0');
-// btn0.addEventListener('click', displayFunction(7));
-
-let box8 = document.getElementsByClassName('numbers')[1];
-box8.textContent = '8';
-let box9 = document.getElementsByClassName('numbers')[2];
-box9.textContent = '9';
-let boxDiv = document.getElementsByClassName('numbers')[3];
-boxDiv.textContent = '/';
-let box4 = document.getElementsByClassName('numbers')[4];
-box4.textContent = '4';
-let box5 = document.getElementsByClassName('numbers')[5];
-box5.textContent = '5';
-let box6 = document.getElementsByClassName('numbers')[6];
-box6.textContent = '6';
-let boxMult = document.getElementsByClassName('numbers')[7];
-boxMult.textContent = '*';
-let box1 = document.getElementsByClassName('numbers')[8];
-box1.textContent = '1';
-let box2 = document.getElementsByClassName('numbers')[9];
-box2.textContent = '2';
-let box3 = document.getElementsByClassName('numbers')[10];
-box3.textContent = '3';
-let boxSub = document.getElementsByClassName('numbers')[11];
-boxSub.textContent = '-';
-let box0 = document.getElementsByClassName('numbers')[12];
-box0.textContent = '0';
-let boxEquals = document.getElementsByClassName('numbers')[13];
-boxEquals.textContent = '=';
-let boxPlus = document.getElementsByClassName('numbers')[14];
-boxPlus.textContent = '+';
+const btnAdd = document.querySelector('#btnAdd');
+btnAdd.addEventListener('click', () => { displayBox.textContent += '+' });
 
 //////////////////////////////////////////////////////////////
 
-const mybtn = document.querySelector('#mybtn');
-mybtn.addEventListener('click', function(){
-    displayBox.textContent = 'mybtn';
-});
+// const mybtn = document.querySelector('#mybtn');
+// mybtn.addEventListener('click', function(){
+//     displayBox.textContent = 'mybtn';
+// });
 
 cellList = document.querySelectorAll('.numbers'); //cellList is an object + not an array
 
